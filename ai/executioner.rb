dec077676a41d92eacbb365ff5eed41ae2dca3e0
kubicle <kubicle@yahoo.com>
2014-02-09 17:43:38 +0900

    Executioner now seeing saved allies

--- ai/executioner.rb
+++ ai/executioner.rb
@@ -10,12 +10,19 @@ class Executioner < Heuristic
 
   def eval_move(i,j)
     stone = @goban.stone_at?(i,j)
-    threat = 0
+    threat = saving = 0
     stone.unique_enemies(@color).each do |g|
-      threat += g.stones.size if g.lives == 1
+      next if g.lives > 1 # NB: more than 1 is a job for hunter
+      threat += g.stones.size
+      g.all_enemies.each do |ally|
+        next if ally.lives > 1
+        saving += ally.stones.size
+      end
     end
-    $log.debug("Executioner heuristic found a threat of #{threat} at #{i},#{j}") if $debug and threat>0
-    return threat
+    return 0 if threat == 0
+    $log.debug("Executioner heuristic found a threat of #{threat} at #{i},#{j}") if $debug
+    $log.debug("...this would also save #{saving}") if $debug and saving > 0
+    return threat + saving
   end
 
 end
