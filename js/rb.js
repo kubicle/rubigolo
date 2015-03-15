781cea86c0b1a2c62c1e81ca2a45a8aa0e9b4bd4
kubicle <kubicle@yahoo.com>
2015-03-15 19:32:45 +0900

    Correct implem for chop and chomp

--- js/rb.js
+++ js/rb.js
@@ -5,15 +5,29 @@ var main = require('./main');
 
 //--- String
 
+function hasLf(s) {
+    if (s.slice(-1) !== '\n') return 0;
+    if (s.slice(-2) !== '\r\n') return 1;
+    return 2;
+}
+
 String.prototype.chomp = function (tail) {
-  if (!tail) {
-    return this.substr(0, this.length - 1);
+  if (tail === undefined) {
+    return this.chop(hasLf(this)); // NB: we cut 0 if no LF
   }
   var pos = this.length - tail.length;
   if (this.substr(pos) === tail) {
     return this.substr(0, pos);
   }
-  return this.toString();
+  return this.toString(); // unchanged string is returned
+};
+
+String.prototype.chop = function (count) {
+  if (count === undefined) {
+    // special behavior: chop ending \n or \r\n
+    count = hasLf(this) || 1; // we cut at least 1
+  }
+  return this.substr(0, this.length - count);
 };
 
 String.prototype.startWith = function (head) {
