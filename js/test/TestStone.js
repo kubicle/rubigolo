c062c0dc1885bfc8ebac39d5f965ca0104e0cb95
kubicle <kubicle@yahoo.com>
2015-04-01 01:28:00 +0900

    More camelCase

--- js/test/TestStone.js
+++ js/test/TestStone.js
@@ -17,85 +17,82 @@ function TestStone(testName) {
 inherits(TestStone, main.TestCase);
 module.exports = main.tests.add(TestStone);
 
-
-// NB: for debugging think of using @goban.console_display
-TestStone.prototype.init_board = function () {
+TestStone.prototype.initBoard = function () {
     this.goban = new Goban(5);
 };
 
-TestStone.prototype.how_many_lives = function (i, j) {
-    var s = this.goban.stone_at(i, j);
-    var lives_before = s.empties().length;
+TestStone.prototype.howManyLives = function (i, j) {
+    var s = this.goban.stoneAt(i, j);
+    var livesBefore = s.empties().length;
     // we test the play/undo too
-    s = Stone.play_at(this.goban, i, j, main.WHITE);
+    s = Stone.playAt(this.goban, i, j, main.WHITE);
     var lives = s.empties().length;
-    assert_equal(lives_before, lives);
+    assertEqual(livesBefore, lives);
     Stone.undo(this.goban);
-    var lives_after = s.empties().length;
-    assert_equal(lives_after, lives);
+    var livesAfter = s.empties().length;
+    assertEqual(livesAfter, lives);
     return lives;
 };
 
 // Not very useful anymore for stones
-TestStone.prototype.test_how_many_lives = function () {
-    assert_equal(2, this.how_many_lives(1, 1));
-    assert_equal(2, this.how_many_lives(this.goban.gsize, this.goban.gsize));
-    assert_equal(2, this.how_many_lives(1, this.goban.gsize));
-    assert_equal(2, this.how_many_lives(this.goban.gsize, 1));
-    assert_equal(4, this.how_many_lives(2, 2));
-    assert_equal(4, this.how_many_lives(this.goban.gsize - 1, this.goban.gsize - 1));
-    var s = Stone.play_at(this.goban, 2, 2, main.BLACK); // we will try white stones around this one
+TestStone.prototype.testHowManyLives = function () {
+    assertEqual(2, this.howManyLives(1, 1));
+    assertEqual(2, this.howManyLives(this.goban.gsize, this.goban.gsize));
+    assertEqual(2, this.howManyLives(1, this.goban.gsize));
+    assertEqual(2, this.howManyLives(this.goban.gsize, 1));
+    assertEqual(4, this.howManyLives(2, 2));
+    assertEqual(4, this.howManyLives(this.goban.gsize - 1, this.goban.gsize - 1));
+    var s = Stone.playAt(this.goban, 2, 2, main.BLACK); // we will try white stones around this one
     var g = s.group;
-    assert_equal(2, this.how_many_lives(1, 1));
-    assert_equal(4, g.lives);
-    assert_equal(2, this.how_many_lives(1, 2));
-    assert_equal(4, g.lives); // verify the live count did not change
-    assert_equal(2, this.how_many_lives(2, 1));
-    assert_equal(3, this.how_many_lives(2, 3));
-    assert_equal(3, this.how_many_lives(3, 2));
-    return assert_equal(4, this.how_many_lives(3, 3));
+    assertEqual(2, this.howManyLives(1, 1));
+    assertEqual(4, g.lives);
+    assertEqual(2, this.howManyLives(1, 2));
+    assertEqual(4, g.lives); // verify the live count did not change
+    assertEqual(2, this.howManyLives(2, 1));
+    assertEqual(3, this.howManyLives(2, 3));
+    assertEqual(3, this.howManyLives(3, 2));
+    return assertEqual(4, this.howManyLives(3, 3));
 };
 
-TestStone.prototype.test_play_at = function () {
+TestStone.prototype.testPlayAt = function () {
     // single stone
-    var s = Stone.play_at(this.goban, 5, 4, main.BLACK);
-    assert_equal(s, this.goban.stone_at(5, 4));
-    assert_equal(this.goban, s.goban);
-    assert_equal(main.BLACK, s.color);
-    assert_equal(5, s.i);
-    return assert_equal(4, s.j);
+    var s = Stone.playAt(this.goban, 5, 4, main.BLACK);
+    assertEqual(s, this.goban.stoneAt(5, 4));
+    assertEqual(this.goban, s.goban);
+    assertEqual(main.BLACK, s.color);
+    assertEqual(5, s.i);
+    return assertEqual(4, s.j);
 };
 
-TestStone.prototype.test_suicide = function () {
+TestStone.prototype.testSuicide = function () {
     // a2 b2 b1 a3 pass c1
-    Stone.play_at(this.goban, 1, 2, main.BLACK);
-    Stone.play_at(this.goban, 2, 2, main.WHITE);
-    Stone.play_at(this.goban, 2, 1, main.BLACK);
-    assert_equal(false, Stone.valid_move(this.goban, 1, 1, main.WHITE)); // suicide invalid
-    Stone.play_at(this.goban, 1, 3, main.WHITE);
-this.goban.debug_display();
-    assert_equal(true, Stone.valid_move(this.goban, 1, 1, main.WHITE)); // now this would be a kill
-    assert_equal(true, Stone.valid_move(this.goban, 1, 1, main.BLACK)); // black could a1 too (merge)
-    Stone.play_at(this.goban, 3, 1, main.WHITE); // now 2 black stones share a last life
-    return assert_equal(false, Stone.valid_move(this.goban, 1, 1, main.BLACK)); // so this would be a suicide with merge
+    Stone.playAt(this.goban, 1, 2, main.BLACK);
+    Stone.playAt(this.goban, 2, 2, main.WHITE);
+    Stone.playAt(this.goban, 2, 1, main.BLACK);
+    assertEqual(false, Stone.validMove(this.goban, 1, 1, main.WHITE)); // suicide invalid
+    Stone.playAt(this.goban, 1, 3, main.WHITE);
+    assertEqual(true, Stone.validMove(this.goban, 1, 1, main.WHITE)); // now this would be a kill
+    assertEqual(true, Stone.validMove(this.goban, 1, 1, main.BLACK)); // black could a1 too (merge)
+    Stone.playAt(this.goban, 3, 1, main.WHITE); // now 2 black stones share a last life
+    return assertEqual(false, Stone.validMove(this.goban, 1, 1, main.BLACK)); // so this would be a suicide with merge
 };
 
-TestStone.prototype.test_ko = function () {
+TestStone.prototype.testKo = function () {
     // pass b2 a2 a3 b1 a1
-    Stone.play_at(this.goban, 2, 2, main.WHITE);
-    Stone.play_at(this.goban, 1, 2, main.BLACK);
-    Stone.play_at(this.goban, 1, 3, main.WHITE);
-    Stone.play_at(this.goban, 2, 1, main.BLACK);
-    Stone.play_at(this.goban, 1, 1, main.WHITE); // kill!
-    assert_equal(false, Stone.valid_move(this.goban, 1, 2, main.BLACK)); // now this is a ko
-    Stone.play_at(this.goban, 4, 4, main.BLACK); // play once anywhere else
-    Stone.play_at(this.goban, 4, 5, main.WHITE);
-    assert_equal(true, Stone.valid_move(this.goban, 1, 2, main.BLACK)); // ko can be taken by black
-    Stone.play_at(this.goban, 1, 2, main.BLACK); // black takes the ko
-    assert_equal(false, Stone.valid_move(this.goban, 1, 1, main.WHITE)); // white cannot take the ko
-    Stone.play_at(this.goban, 5, 5, main.WHITE); // play once anywhere else
-    Stone.play_at(this.goban, 5, 4, main.BLACK);
-    assert_equal(true, Stone.valid_move(this.goban, 1, 1, main.WHITE)); // ko can be taken back by white
-    Stone.play_at(this.goban, 1, 1, main.WHITE); // white takes the ko
-    return assert_equal(false, Stone.valid_move(this.goban, 1, 2, main.BLACK)); // and black cannot take it now
+    Stone.playAt(this.goban, 2, 2, main.WHITE);
+    Stone.playAt(this.goban, 1, 2, main.BLACK);
+    Stone.playAt(this.goban, 1, 3, main.WHITE);
+    Stone.playAt(this.goban, 2, 1, main.BLACK);
+    Stone.playAt(this.goban, 1, 1, main.WHITE); // kill!
+    assertEqual(false, Stone.validMove(this.goban, 1, 2, main.BLACK)); // now this is a ko
+    Stone.playAt(this.goban, 4, 4, main.BLACK); // play once anywhere else
+    Stone.playAt(this.goban, 4, 5, main.WHITE);
+    assertEqual(true, Stone.validMove(this.goban, 1, 2, main.BLACK)); // ko can be taken by black
+    Stone.playAt(this.goban, 1, 2, main.BLACK); // black takes the ko
+    assertEqual(false, Stone.validMove(this.goban, 1, 1, main.WHITE)); // white cannot take the ko
+    Stone.playAt(this.goban, 5, 5, main.WHITE); // play once anywhere else
+    Stone.playAt(this.goban, 5, 4, main.BLACK);
+    assertEqual(true, Stone.validMove(this.goban, 1, 1, main.WHITE)); // ko can be taken back by white
+    Stone.playAt(this.goban, 1, 1, main.WHITE); // white takes the ko
+    return assertEqual(false, Stone.validMove(this.goban, 1, 2, main.BLACK)); // and black cannot take it now
 };
