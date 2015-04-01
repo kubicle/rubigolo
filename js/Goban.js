f3988873d5ff4d4d54d1ee362049aecb38afd0a2
kubicle <kubicle@yahoo.com>
2015-04-02 01:00:06 +0900

    More camelCase

--- js/Goban.js
+++ js/Goban.js
@@ -11,32 +11,32 @@ var Group = require('./Group');
 // - It also remembers the list of stones played and can share this info for undo feature.
 // - For console game and debug features, a goban can also "draw" its content as text.
 // See Stone and Group classes for the layer above this.
-//public read-only attribute: gsize, grid, scoring_grid, merged_groups, killed_groups, garbage_groups;
+//public read-only attribute: gsize, grid, scoringGrid, mergedGroups, killedGroups, garbageGroups;
 
 /** @class */
 function Goban(gsize) {
     if (gsize === undefined) gsize = 19;
     this.gsize = gsize;
     this.grid = new Grid(gsize);
-    this.scoring_grid = new Grid(gsize);
+    this.scoringGrid = new Grid(gsize);
     this.ban = this.grid.yx;
     var i, j;
-    for (j = 1; j <= gsize; j++) {
-        for (i = 1; i <= gsize; i++) {
+    for (var j = 1; j <= gsize; j++) {
+        for (var i = 1; i <= gsize; i++) {
             this.ban[j][i] = new Stone(this, i, j, main.EMPTY);
         }
     }
-    for (j = 1; j <= gsize; j++) {
-        for (i = 1; i <= gsize; i++) {
-            this.ban[j][i].find_neighbors();
+    for (var j = 1; j <= gsize; j++) {
+        for (var i = 1; i <= gsize; i++) {
+            this.ban[j][i].findNeighbors();
         }
     }
     // sentinel for group list searches; NB: values like -100 helps detecting bugs when value is used by mistake
     Goban.sentinel = new Group(this, new Stone(this, -50, -50, main.EMPTY), -100, 0);
-    this.killed_groups = [Goban.sentinel]; // so that we can always do @killed_groups.last.color, etc.
-    this.merged_groups = [Goban.sentinel];
-    this.garbage_groups = [];
-    this.num_groups = 0;
+    this.killedGroups = [Goban.sentinel]; // so that we can always do @killed_groups.last.color, etc.
+    this.mergedGroups = [Goban.sentinel];
+    this.garbageGroups = [];
+    this.numGroups = 0;
     this.history = [];
 }
 module.exports = Goban;
@@ -52,42 +52,44 @@ Goban.prototype.clear = function () {
         }
     }
     // Collect all the groups and put them into @garbage_groups
-    this.killed_groups.shift(); // removes @@sentinel
-    this.merged_groups.shift(); // removes @@sentinel
-    this.garbage_groups.concat(this.killed_groups);
-    this.garbage_groups.concat(this.merged_groups);
-    this.killed_groups.clear();
-    this.killed_groups.push(Goban.sentinel);
-    this.merged_groups.clear();
-    this.merged_groups.push(Goban.sentinel);
-    this.num_groups = 0;
+    this.killedGroups.shift(); // removes @@sentinel
+    this.mergedGroups.shift(); // removes @@sentinel
+    this.garbageGroups.concat(this.killedGroups);
+    this.garbageGroups.concat(this.mergedGroups);
+    this.killedGroups.clear();
+    this.killedGroups.push(Goban.sentinel);
+    this.mergedGroups.clear();
+    this.mergedGroups.push(Goban.sentinel);
+    this.numGroups = 0;
     return this.history.clear();
 };
 
 // Allocate a new group or recycles one from garbage list.
 // For efficiency, call this one, do not call the regular Group.new method.
-Goban.prototype.new_group = function (stone, lives) {
-    var group = this.garbage_groups.pop();
+Goban.prototype.newGroup = function (stone, lives) {
+    var group = this.garbageGroups.pop();
     if (group) {
         return group.recycle(stone, lives);
     } else {
-        this.num_groups += 1;
-        return new Group(this, stone, lives, this.num_groups);
+        this.numGroups += 1;
+        return new Group(this, stone, lives, this.numGroups);
     }
 };
 
 Goban.prototype.image = function () {
-    return this.grid.to_text2(false, ',', function (s) {
+    return this.grid.toTextExt(false, ',', function (s) {
         return Grid.COLOR_CHARS[s.color];
     }).chop();
 };
 
 // For debugging only
-Goban.prototype.debug_display = function () {
+Goban.prototype.debugDisplay = function () {
     console.log('Board:');
-    console.log(this.grid.to_text());
+    console.log(this.grid.toText(function (s) {
+        return Grid.COLOR_CHARS[s.color];
+    }));
     console.log('Groups:');
-    console.log(this.grid.to_text(function (s) {
+    console.log(this.grid.toText(function (s) {
         if (s.group) {
             return '' + s.group.ndx;
         } else {
@@ -103,38 +105,38 @@ Goban.prototype.debug_display = function () {
             }
         }
     }
-    for (var ndx = 1; ndx <= this.num_groups; ndx++) {
+    for (var ndx = 1; ndx <= this.numGroups; ndx++) {
         if (groups[ndx]) {
-            console.log(groups[ndx].debug_dump());
+            console.log(groups[ndx].debugDump());
         }
     }
 };
 
 // This display is for debugging and text-only game
-Goban.prototype.console_display = function () {
-    console.log(this.grid.to_text(function (s) {
+Goban.prototype.consoleDisplay = function () {
+    console.log(this.grid.toText(function (s) {
         return Grid.COLOR_CHARS[s.color];
     }));
 };
 
 // Basic validation only: coordinates and checks the intersection is empty
 // See Stone class for evolved version of this (calling this one)
-Goban.prototype.valid_move = function (i, j) {
+Goban.prototype.validMove = function (i, j) {
     if (i < 1 || i > this.gsize || j < 1 || j > this.gsize) {
         return false;
     }
     return this.ban[j][i].empty();
 };
 
-Goban.prototype.stone_at = function (i, j) {
+Goban.prototype.stoneAt = function (i, j) {
     return this.ban[j][i];
 };
 
 Goban.prototype.color = function (i, j) {
     var stone = this.ban[j][i];
-    if (stone) {
+    if (stone) { // works because BORDER == nil
         return stone.color;
-    } // works because BORDER == nil
+    }
     return main.BORDER;
 };
 
@@ -143,13 +145,13 @@ Goban.prototype.empty = function (i, j) {
     return this.ban[j][i].empty();
 };
 
-Goban.prototype.move_number = function () {
+Goban.prototype.moveNumber = function () {
     return this.history.length;
 };
 
 // Plays a stone and stores it in history
 // Actually we simply return the existing stone and the caller will update it
-Goban.prototype.play_at = function (i, j) {
+Goban.prototype.playAt = function (i, j) {
     var stone = this.ban[j][i];
     if (stone.color !== main.EMPTY) {
         throw new Error('Tried to play on existing stone in ' + i + ',' + j);
@@ -164,6 +166,8 @@ Goban.prototype.undo = function () {
     return this.history.pop();
 };
 
-Goban.prototype.previous_stone = function () {
+Goban.prototype.previousStone = function () {
     return this.history[this.history.length-1];
 };
+
+// E02: unknown method concat(...)
