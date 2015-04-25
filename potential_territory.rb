05ca82e1a556e014f7e76377e03720569fb021a9
kubicle <kubicle@yahoo.com>
2015-04-25 11:36:34 +0900

    sprintf change (github hicup)

--- potential_territory.rb
+++ potential_territory.rb
@@ -36,6 +36,7 @@ class PotentialTerritory
         @territory.yx[j][i] = owner / 2.0
       end
     end
+    $log.debug("\n+1=white, -1=black, 0=no one\n"+@territory.to_text{|v| v==0 ? "    0" : "#{'%+.1f' % v}"}) if $debug
     return @territory.yx
   end
 
