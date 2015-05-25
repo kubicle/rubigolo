910766e52fd7c125bc22df77c15fb3a603ae7b66
kubicle <kubicle@yahoo.com>
2015-05-26 00:40:45 +0900

    More clever Pusher (WIP). Try to limit to connectable pushes

--- js/ai/Pusher.js
+++ js/ai/Pusher.js
@@ -3,12 +3,14 @@
 
 var inherits = require('util').inherits;
 var main = require('../main');
-// Quite a dumb way of "pushing" our influence further...
-// For that reason the coeff are rather low.
-// This should eventually disappear.
 var Heuristic = require('./Heuristic');
 
-/** @class */
+
+/** @class
+ *  Quite a dumb way of "pushing" our influence further...
+ *  For that reason the coeff are rather low.
+ *  This should eventually disappear.
+ */
 function Pusher(player) {
     Heuristic.call(this, player);
     this.allyCoeff = this.getGene('ally-infl', 0.1, 0.01, 4.0);
@@ -24,9 +26,45 @@ Pusher.prototype.evalMove = function (i, j) {
     if (enemyInf === 0 || allyInf === 0) {
         return 0;
     }
+    if (!this.canConnect(i, j, this.color)) return 0;
+
     var score = 0.33 * (this.enemyCoeff * enemyInf - this.allyCoeff * allyInf);
     if (main.debug) {
         main.log.debug('Pusher heuristic sees influences ' + allyInf + ' - ' + enemyInf + ' at ' + i + ',' + j + ' -> ' + '%.03f'.format(score));
     }
     return score;
 };
+
+/** @return a group if a new stone at i,j will be able to connect with a "color" group around.
+ *  Basically this is to make sure i,j is not alone (and not to see if i,j is a connector!) */
+// +@+
+// O+O
+// @*@ <-- TODO review this case; looks like white here cannot connect
+Pusher.prototype.canConnect = function (i, j, color) {
+    var stone = this.goban.stoneAt(i,j);
+
+    // first look around for empties and allies (a single ally means we connect!)
+    var empties = [];
+    for (var nNdx = stone.neighbors.length - 1; nNdx >= 0; nNdx--) {
+        var n = stone.neighbors[nNdx];
+        if (n.color === color && n.group.lives > 1) return n.group;
+        if (n.color === main.EMPTY) empties.push(n);
+    }
+
+    // look around each empty for allies
+    var candidate = null;
+    for(var eNdx = empties.length - 1; eNdx >= 0; eNdx--) {
+        var empty = empties[eNdx];
+        for (var n2Ndx = empty.neighbors.length - 1; n2Ndx >= 0; n2Ndx--) {
+            var en = empty.neighbors[n2Ndx];
+            if (en === stone || en.color !== color || en.group.lives < 2) continue;
+            if (stone.distance(en) > 1) continue;
+            if (candidate) {
+                return candidate; // or en? TODO test me
+            }
+            candidate = en;
+        }
+    }
+    return false;
+};
+
