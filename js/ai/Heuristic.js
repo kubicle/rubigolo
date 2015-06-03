0ce49effaa7734062f71e84a9527a91d12308519
kubicle <kubicle@yahoo.com>
2015-06-03 23:57:56 +0900

    canConnect & distanceBetweenStones moved to Heuristic base class

--- js/ai/Heuristic.js
+++ js/ai/Heuristic.js
@@ -1,15 +1,17 @@
 //Translated from heuristic.rb using babyruby2js
 'use strict';
 
+var main = require('../main');
+var Grid = require('../Grid');
+
 
 /** @class Base class for all heuristics.
  *  Anything useful for all of them should be stored as data member here.
  *  public read-only attribute: negative
  */
 function Heuristic(player, consultant) {
-    if (consultant === undefined) consultant = false;
     this.player = player;
-    this.consultant = consultant;
+    this.consultant = !!consultant;
     this.negative = false;
     this.goban = player.goban;
     this.gsize = player.goban.gsize;
@@ -39,8 +41,6 @@ Heuristic.prototype.setAsNegative = function () {
 };
 
 Heuristic.prototype.getGene = function (name, defVal, lowLimit, highLimit) {
-    if (lowLimit === undefined) lowLimit = null;
-    if (highLimit === undefined) highLimit = null;
     return this.player.genes.get(this.constructor.name + '-' + name, defVal, lowLimit, highLimit);
 };
 
@@ -53,4 +53,100 @@ Heuristic.prototype.groupThreat = function (g) {
     }
     return g.stones.length * 2 + // 2 points are pretty much granted for the prisonners
         this.spaceInvasionCoeff * numEmpties; //...and the "open gate" to territory will count a lot
+};
+
+Heuristic.prototype.markMoveAsBlunder = function (reason) {
+    this.player.markMoveAsBlunder(this.constructor.name + ':' + reason);
+};
+
+Heuristic.prototype.distanceFromStoneToBorder = function (stone) {
+    var gsize = this.goban.gsize;
+    var i = stone.i, j = stone.j;
+    return Math.min(Math.min(i - 1, gsize - i), Math.min(j - 1, gsize - j));
+};
+
+Heuristic.prototype.diagonalStones = function (s1, s2) {
+    return [this.goban.stoneAt(s1.i, s2.j), this.goban.stoneAt(s2.i, s1.j)];
+};
+
+Heuristic.prototype.distanceBetweenStones = function (s1, s2, color) {
+    var dx = Math.abs(s2.i - s1.i), dy = Math.abs(s2.j - s1.j);
+    if (dx + dy === 1) return 0; // already connected
+    var enemy = 1 - color;
+    var numEnemies = 0, between;
+    if (dx === 1 && dy === 1) { // hane
+        var diags = this.diagonalStones(s1, s2);
+        if (diags[0].color === color || diags[1].color === color) return 0;
+        if (diags[0].color === enemy) numEnemies++;
+        if (diags[1].color === enemy) numEnemies++;
+        if (numEnemies === 0) return 0; // safe hane
+        if (numEnemies === 2) return 9; // cut!
+        if (this.distanceFromStoneToBorder(s1) === 0 || this.distanceFromStoneToBorder(s2) === 0) {
+            // hane close to border works even if 1 enemy unless another enemy is waiting on the border
+            var empty = diags[0].color === enemy ? diags[1] : diags[0];
+            if (empty.allyStones(enemy) !== 0) return 1;
+            return 0;
+        }
+        return 1;
+    }
+    if (dx + dy === 2) {
+        between = this.goban.stoneAt((s1.i + s2.i) / 2, (s1.j + s2.j) /2);
+        if (between.color === color) return 0; // already connected
+        if (between.color === enemy) return between.group.lives; // REVIEW ME
+        for (var i = between.neighbors.length - 1; i >= 0; i--) {
+            if (between.neighbors[i].color === enemy) numEnemies++;
+        }
+        if (numEnemies >= 1) return 1; // needs 1 move to connect (1 or 2 enemies is same)
+        if (this.distanceFromStoneToBorder(s1) + this.distanceFromStoneToBorder(s2) === 0) {
+            return 0; // along border with 0 enemy around is safe
+        }
+        return 0.5; // REVIEW ME
+    }
+    var d1 = this.distanceFromStoneToBorder(s1), d2 = this.distanceFromStoneToBorder(s2);
+    if (dx + dy === 3 && d1 === 0 && d2 === 0) {
+        // TODO code betweenStones and test it
+        var betweens = this.betweenStones(s1, s2);
+        var dist = 0;
+        for (var b = betweens.length - 1; b >= 0; b--) {
+            between = betweens[b];
+            if (between.color === enemy) dist += between.group.lives;
+            if (between.allyStones(enemy) !== 0) dist += 1;
+        }
+        return dist;
+    }
+    //TODO: add other cases like monkey-jump
+    return dx + dy;
+};
+
+/** Evaluates if a new stone at i,j will be able to connect with a "color" group around.
+ *  Basically this is to make sure i,j is not alone (and not to see if i,j is a connector!) */
+// +@+
+// O+O
+// @*@ <-- TODO review this case; looks like white here cannot connect
+Heuristic.prototype.canConnect = function (i, j, color) {
+    var stone = this.goban.stoneAt(i,j);
+
+    // first look around for empties and allies (a single ally means we connect!)
+    var empties = [];
+    for (var nNdx = stone.neighbors.length - 1; nNdx >= 0; nNdx--) {
+        var n = stone.neighbors[nNdx];
+        if (n.color === color && n.group.lives > 1) return n;
+        if (n.color === main.EMPTY) empties.push(n);
+    }
+    // look around each empty for allies
+    var moveNeeded = 2;
+    for(var eNdx = empties.length - 1; eNdx >= 0; eNdx--) {
+        var empty = empties[eNdx];
+        for (var n2Ndx = empty.neighbors.length - 1; n2Ndx >= 0; n2Ndx--) {
+            var en = empty.neighbors[n2Ndx];
+            if (en === stone) continue; // same stone
+            if (en.color !== color) continue; // empty or enemy
+            if (en.group.lives === 1) continue; // TODO: look better at group's health
+            var dist = this.distanceBetweenStones(stone, en, color);
+            if (dist >= 2) continue;
+            moveNeeded -= (2 - dist);
+            if (moveNeeded <= 0.5) return en; // REVIEW ME
+        }
+    }
+    return null;
 };
