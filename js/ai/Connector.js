6c2b46d7a7e2d1ad3437677ee97dd3ab63cc7261
kubicle <kubicle@yahoo.com>
2015-06-04 00:19:35 +0900

    Better Connector logic

--- js/ai/Connector.js
+++ js/ai/Connector.js
@@ -4,9 +4,10 @@
 var inherits = require('util').inherits;
 var main = require('../main');
 var Heuristic = require('./Heuristic');
+var Grid = require('../Grid');
+
 
 /** @class A move that connects 2 of our groups is good.
- *  TODO: this could threaten our potential for keeping eyes, review this.
  */
 function Connector(player) {
     Heuristic.call(this, player);
@@ -18,52 +19,59 @@ inherits(Connector, Heuristic);
 module.exports = Connector;
 
 Connector.prototype.evalMove = function (i, j) {
-    // we care a lot if the enemy is able to cut us,
-    // and even more if by connecting we cut them...
-    // TODO: the opposite heuristic - a cutter; and make both more clever.
+    return this.connectsMyGroups(i, j, this.color) +
+           this.connectsMyGroups(i, j, 1 - this.color);
+};
+
+Connector.prototype.connectsMyGroups = function (i, j, color) {
     // TODO: one other way to connect 2 groups is to "protect" the cutting point; handle this here
     var stone = this.goban.stoneAt(i, j);
-    var allies = stone.uniqueAllies(this.color);
-    var numGroups = allies.length;
-
-    if (numGroups < 2) return 0; // nothing to connect here
-
-    var s1, s2; // we will get stone 1 & 2 for cases with 2 groups connecting
+    var s1, s1b, s2, s2b, s3;
     var numStones = 0, numEnemies = 0;
-    for (var s, s_array = stone.neighbors, s_ndx = 0; s=s_array[s_ndx], s_ndx < s_array.length; s_ndx++) {
-        switch (s.group) {
-        case null: continue;
-        case allies[0]: s1 = s; break;
-        case allies[1]: s2 = s;
+    for (var n = stone.neighbors.length - 1; n >= 0; n--) {
+        var s = stone.neighbors[n];
+        switch (s.color) {
+        case main.EMPTY: continue;
+        case color:
+            numStones++;
+            if (!s1) {
+                s1 = s;
+            } else if (!s2) {
+                if (s.group !== s1.group) s2 = s; else s1b = s;
+            } else {
+                if (s.group !== s2.group) s3 = s; else s2b = s;
+            }
+            break;
+        default: numEnemies++;
         }
-        if (s.color === this.color) numStones++;
-        else numEnemies++;
     }
-    if (numStones === 4) return 0;
+    if (!s2) return 0; // nothing to connect here
+    if (numStones === 4) return 0; // 1 empty between 4 stones; never connect unless forced to
     // 3 of our stones around: no need to connect unless enemy comes by
-    if (numGroups === 3 && numEnemies === 0) return 0;
+    if (numStones === 3 && numEnemies === 0) return 0;
 
-    // Case of diagonal (strong) stones (TODO: handle the case with a 3rd stone in same group than 1 or 2)
-    if (numStones === 2 && s1.i !== s2.i && s1.j !== s2.j) {
+    var numGroups = s3 ? 3 : 2;
+    // if 3rd stone in same group than 1 or 2; we keep the diagonal ones
+    if (numGroups === 2 && numStones === 3) {
+        if (s2b) { s1b = s2b; var swap = s1; s1 = s2; s2 = swap; }
+        if (s1.i === s2.i || s1.j === s2.j) s1 = s1b;
+    }
+    // Case of diagonal (strong) stones
+    if (s1.i !== s2.i && s1.j !== s2.j) {
         // No need to connect if both connection points are free (no cutting stone yet)
-        if (this.goban.empty(s1.i, s2.j) && this.goban.empty(s2.i, s1.j)) return 0;
-        // We count the cutting stone as enemy
+        if (this.goban.isEmpty(s1.i, s2.j) && this.goban.isEmpty(s2.i, s1.j)) return 0;
+        // We count the cutting stone as enemy (we did not "see" it above because it's diagonal)
         numEnemies++;
     }
-
-    switch (numEnemies) {
-    case 0:
-        var _eval = this.inflCoeff / this.inf.map[j][i][this.color];
-        break;
-    case 1:
-        _eval = this.allyCoeff1 * numGroups;
-        break;
-    default: 
-        _eval = this.allyCoeff2 * numGroups;
+    var score;
+    if (numEnemies === 0) {
+        score = this.inflCoeff / this.inf.map[j][i][color];
+    } else {
+        score = this.allyCoeff1 * numGroups;
     }
     if (main.debug) {
-        main.log.debug('Connector gives ' + '%.2f'.format(_eval) + ' to ' + i + ',' + j +
+        main.log.debug('Connector for ' + Grid.colorName(color) + ' gives ' + score.toFixed(3) + ' to ' + i + ',' + j +
             ' (allies:' + numGroups + ' enemies: ' + numEnemies + ')');
     }
-    return _eval;
+    return score;
 };
