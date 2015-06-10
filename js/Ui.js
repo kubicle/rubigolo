bbb0c56c2e76c1797a07d7b43a371b168616d84c
kubicle <kubicle@yahoo.com>
2015-06-10 21:47:08 +0900

    UI: AI can play 0, 1, 2 players + new "Eval" button for tests (WIP)

--- js/Ui.js
+++ js/Ui.js
@@ -9,11 +9,13 @@ var Grid = main.Grid;
 var Ai1Player = main.Ai1Player;
 var ScoreAnalyser = main.ScoreAnalyser;
 
+var WHITE = main.WHITE, BLACK = main.BLACK, EMPTY = main.EMPTY;
+
 
 function Ui() {
   this.gsize = 9;
   this.handicap = 0;
-  this.aiColor = main.WHITE;
+  this.aiPlays = 'white';
   this.withCoords = true;
 
   this.scorer = new ScoreAnalyser();
@@ -34,19 +36,21 @@ Ui.prototype.createBoard = function () {
   var self = this;
   this.board.addEventListener('click', function (x,y) {
     if (x < 0 || y < 0 || x >= self.gsize || y >= self.gsize) return;
-    self.onClick(x + 1, self.gsize - y);
+    var move = Grid.moveAsString(x + 1, self.gsize - y);
+    if (self.inEvalMode) return self.evalMove(move);
+    self.playerMove(move);
   });
 };
 
 // Color codes conversions WGo->Rubigolo
 var fromWgoColor = {};
-fromWgoColor[WGo.B] = main.BLACK;
-fromWgoColor[WGo.W] = main.WHITE;
+fromWgoColor[WGo.B] = BLACK;
+fromWgoColor[WGo.W] = WHITE;
 // Color codes conversions Rubigolo->WGo
 var toWgoColor = {};
-toWgoColor[main.EMPTY] = null;
-toWgoColor[main.BLACK] = WGo.B;
-toWgoColor[main.WHITE] = WGo.W;
+toWgoColor[EMPTY] = null;
+toWgoColor[BLACK] = WGo.B;
+toWgoColor[WHITE] = WGo.W;
 
 Ui.prototype.refreshBoard = function (force) {
   this.refreshHistory();
@@ -69,22 +73,24 @@ Ui.prototype.refreshBoard = function (force) {
 };
 
 Ui.prototype.showScoringBoard = function () {
+  this.refreshBoard(); // the base is the up-to-date board
+  
   for (var j = 0; j < this.gsize; j++) {
     for (var i = 0; i < this.gsize; i++) {
       var color = this.game.goban.scoringGrid.yx[this.gsize - j][i + 1];
       switch (color) {
-      case Grid.TERRITORY_COLOR + main.BLACK:
-      case Grid.DEAD_COLOR + main.WHITE:
+      case Grid.TERRITORY_COLOR + BLACK:
+      case Grid.DEAD_COLOR + WHITE:
         this.board.addObject({ x: i, y: j, type: 'mini', c: WGo.B });
         break;
-      case Grid.TERRITORY_COLOR + main.WHITE:
-      case Grid.DEAD_COLOR + main.BLACK:
+      case Grid.TERRITORY_COLOR + WHITE:
+      case Grid.DEAD_COLOR + BLACK:
         this.board.addObject({ x: i, y: j, type: 'mini', c: WGo.W });
         break;
       case Grid.DAME_COLOR:
         this.board.addObject({ x: i, y: j, type: 'SL', c: 'grey' });
         break;
-      case main.BLACK: case main.WHITE: break;
+      case BLACK: case WHITE: break;
       case Grid.EMPTY_COLOR: break;
       default: console.error(i,j,color);
       }
@@ -135,20 +141,20 @@ Ui.prototype.newGameDialog = function () {
   var options = form.appendChild(newElement('div'));
   var size = newInput(options, 'size', 'Size', this.gsize);
   var handicap = newInput(options, 'handicap', 'Handicap', this.handicap);
-  var aiColor = newInput(options, 'aiColor', 'AI plays', this.aiColor);
+  var aiColor = newInput(options, 'aiColor', 'AI plays', this.aiPlays);
   var moves = newInput(form, 'moves', 'Moves to load');
   var self = this;
   var okBtn = form.appendChild(newButton('start', 'OK', function (ev) {
     ev.preventDefault();
     self.gsize = parseInt(size.value) || 9;
     self.handicap = parseInt(handicap.value) || 0;
-    self.aiColor = parseInt(aiColor.value) ? main.WHITE : main.BLACK;
+    self.aiPlays = aiColor.value;
 
     self.startGame(moves.value);
     document.body.removeChild(dialog);
     self.ctrl.newg.disabled = false;
   }));
-  okBtn.setAttribute('type','submit'); //style="display:none"
+  okBtn.setAttribute('type','submit');
 };
 
 Ui.prototype.newButton = function (name, label, action) {
@@ -160,11 +166,18 @@ Ui.prototype.createControls = function () {
   this.controls = document.getElementById('controls');
   var self = this;
   this.newButton('pass', 'Pass', function () {
-    self.game.playOneMove('pass');
-    self.letAiPlay();
+    self.playerMove('pass');
+  });
+  this.newButton('next', 'Next', function () {
+    self.letNextPlayerPlay();
+  });
+  this.newButton('next10', 'Next 10', function () {
+    self.automaticAiPlay(10);
+  });
+  this.newButton('nextAll', 'Finish', function () {
+    self.automaticAiPlay();
   });
   this.newButton('undo', 'Undo', function () {
-    self.message('Undone AI move and your previous move');
     self.playerMove('undo');
   });
   this.newButton('resi', 'Resign', function () {
@@ -181,12 +194,20 @@ Ui.prototype.createControls = function () {
   this.newButton('newg', 'New game', function () {
     self.newGameDialog();
   });
+  this.newButton('eval', 'Eval move', function () {
+    self.inEvalMode = main.debug = !self.inEvalMode;
+    main.log.level = main.Logger.DEBUG;
+  });
 };
 
 Ui.prototype.toggleControls = function () {
   var inGame = !(this.game.gameEnded || this.game.gameEnding);
+  var auto = this.numberOfAi === 2;
+
   this.setVisible(['accept', 'refuse'], this.game.gameEnding);
-  this.setVisible(['pass', 'undo', 'resi'], inGame);
+  this.setVisible(['undo'], inGame);
+  this.setVisible(['pass', 'resi'], inGame && !auto);
+  this.setVisible(['next', 'next10', 'nextAll'], inGame && auto);
   this.setVisible(['newg'], this.game.gameEnded);
 };
 
@@ -203,19 +224,32 @@ Ui.prototype.message = function (html, append) {
   this.output.innerHTML += html;
 };
 
+Ui.prototype.createPlayers = function () {
+  this.players = [];
+  this.numberOfAi = 0;
+  if (this.aiPlays === 'black' || this.aiPlays === 'both') {
+    this.players[BLACK] = new Ai1Player(this.game.goban, BLACK);
+    this.numberOfAi++;
+  }
+  if (this.aiPlays === 'white' || this.aiPlays === 'both') {
+    this.players[WHITE] = new Ai1Player(this.game.goban, WHITE);
+    this.numberOfAi++;
+  }
+  this.message('Game started. Your turn...'); // erased if moved are played
+};
+
 Ui.prototype.startGame = function (firstMoves) {
   this.createBoard();
   var game = this.game = new GameLogic();
   game.newGame(this.gsize, this.handicap);
-  this.aiPlayer = new Ai1Player(game.goban, this.aiColor);
+  this.createPlayers();
   this.toggleControls();
-  this.message('Game started. Your turn...'); // player will not see this if AI plays below
   this.refreshBoard(); // needed here to clean-up previous score counting if any
   if (firstMoves) {
     game.loadMoves(firstMoves);
     this.refreshBoard();
   }
-  if (this.aiColor === game.curColor) this.letAiPlay();
+  this.letNextPlayerPlay();
 };
 
 /** @return false if game goes on normally; true if special ending action was done */
@@ -238,20 +272,22 @@ Ui.prototype.computeScore = function () {
 Ui.prototype.proposeScore = function () {
   this.computeScore();
   this.message(this.scoreMsg);
-  this.message('<br>Do you accept this score?', true);
+  this.message('<br><br>Do you accept this score?', true);
   this.toggleControls();
   this.refreshHistory();
   this.showScoringBoard();
 };
 
 Ui.prototype.acceptScore = function (acceptEnd) {
-  var humanColor = 1 - this.aiColor; // if dispute, this is always human since AI did the counting
-  this.game.acceptEnding(acceptEnd, humanColor);
+  var whoRefused = this.game.curColor;
+  this.game.acceptEnding(acceptEnd, whoRefused);
   if (acceptEnd) return this.showEnd();
 
   this.message('Score in dispute. Continue playing...');
   this.toggleControls();
   this.refreshBoard(/*force=*/true);
+  // In AI VS AI move we don't ask AI to play again otherwise it simply passes again
+  if (this.numberOfAi < 2) this.letNextPlayerPlay();
 };
 
 Ui.prototype.showEnd = function () {
@@ -260,43 +296,64 @@ Ui.prototype.showEnd = function () {
   this.toggleControls();
 };
 
-Ui.prototype.playerMove = function (move) {
-  if (!this.game.playOneMove(move)) {
-    if (this.game.gameEnded) return; // simply ignore if game already ended
-    return this.message(this.game.getErrors().join('<br>'));
-  }
-  this.refreshBoard();
-  return true;
-};
-
-Ui.prototype.onClick = function (i, j) {
-  var move = Grid.moveAsString(i, j);
-  if (!this.playerMove(move)) return;
-
-  this.letAiPlay();
-};
-
-Ui.prototype.showAiMoveData = function (move) {
-  var txt = 'AI: ' + move + '<br>';
-  txt += this.aiPlayer.getMoveSurveyText(1).replace(/\n/g, '<br>');
-  txt += this.aiPlayer.getMoveSurveyText(2).replace(/\n/g, '<br>');
+Ui.prototype.showAiMoveData = function (aiPlayer, move) {
+  var playerName = Grid.colorName(aiPlayer.color);
+  var txt = playerName + ' (AI): ' + move + '<br>';
+  txt += aiPlayer.getMoveSurveyText(1).replace(/\n/g, '<br>');
+  txt += aiPlayer.getMoveSurveyText(2).replace(/\n/g, '<br>');
   this.message(txt);
 };
 
-Ui.prototype.letAiPlay = function () {
-  // human resigned or double-passed?
-  if (this.checkEnd()) return;
-
-  // get AI move
-  var move = this.aiPlayer.getMove();
+Ui.prototype.letAiPlay = function (aiPlayer, automatic) {
+  var move = aiPlayer.getMove();
   this.game.playOneMove(move);
-  this.showAiMoveData(move);
   
   // AI resigned or double-passed?
   if (this.checkEnd()) return;
 
-  // refresh what should be
-  if (move === 'pass') return this.refreshHistory();
+  if (automatic) return; // no refresh in automatic mode until last move
+  this.showAiMoveData(aiPlayer, move);
+  this.refreshBoard();
+};
+
+Ui.prototype.evalMove = function (move) {
+  var curColor = this.game.curColor;
+  var aiPlayer = new Ai1Player(this.game.goban, curColor);
+  var coords = Grid.parseMove(move);
+  aiPlayer._testMoveEval(coords[0], coords[1]);
+  this.showAiMoveData(aiPlayer, move);
+};
+
+Ui.prototype.playerMove = function (move) {
+  if (this.game.gameEnded) return; // simply ignore if game already ended
+  var playerName = Grid.colorName(this.game.curColor);
+
+  if (!this.game.playOneMove(move)) {
+    return this.message(this.game.getErrors().join('<br>'));
+  }
+  if (this.checkEnd()) return;
+
+  this.refreshBoard();
+  this.message(playerName + ': ' + move);
+  this.letNextPlayerPlay();
+};
+
+Ui.prototype.letNextPlayerPlay = function (automatic) {
+  var curColor = this.game.curColor;
+  var player = this.players[curColor];
+  if (player instanceof Ai1Player) {
+    this.letAiPlay(player, automatic);
+  } else {
+    var playerName = Grid.colorName(curColor);
+    this.message('<br>(' + playerName + '\'s turn)', true);
+  }
+};
+
+Ui.prototype.automaticAiPlay = function (turns) {
+  for(var i = 0; i < turns || !turns; i++) {
+    this.letNextPlayerPlay(true);
+    if (this.game.gameEnding) return; // no refresh since scoring board is displayed
+  }
   this.refreshBoard();
 };
 
