3fa1c0fa1940a761afd568247a255588db31d38b
kubicle <kubicle@yahoo.com>
2015-06-27 13:52:12 +0900

    PotentialTerritory: minor refact + better log

--- js/PotentialTerritory.js
+++ js/PotentialTerritory.js
@@ -6,6 +6,9 @@ var main = require('./main');
 var Stone = require('./Stone');
 var BoardAnalyser = require('./BoardAnalyser');
 
+var EMPTY = main.EMPTY, BLACK = main.BLACK, WHITE = main.WHITE;
+
+
 /** @class */
 function PotentialTerritory(goban) {
     this.goban = goban;
@@ -25,39 +28,25 @@ module.exports = PotentialTerritory;
 // Values in between are possible too.
 PotentialTerritory.prototype.guessTerritories = function () {
     // update real grid to current goban
-    this.realGrid.convert(this.goban.grid);
+    this.realGrid.initFromGoban(this.goban);
     // evaluate 2 "scenarios" - each player plays everywhere *first*
-    for (var first = 0; first < 2; first++) {
+    for (var first = BLACK; first <= WHITE; first++) {
         this.foresee(this.grids[first], first, 1 - first);
     }
-    if (main.debug) {
-        main.log.debug('\nBLACK first:\n' + this.grids[0] + 'WHITE first:\n' + this.grids[1]);
-    }
+    if (main.debug) main.log.debug('\nBLACK first:\n' + this.grids[0] + 'WHITE first:\n' + this.grids[1]);
+
     // now merge the result
     for (var j = 1; j <= this.gsize; j++) {
         for (var i = 1; i <= this.gsize; i++) {
             var owner = 0;
-            for (first = 0; first < 2; first++) {
-                var terrColor = this.grids[first].yx[j][i] - Grid.TERRITORY_COLOR;
-                if (terrColor === main.WHITE) {
-                    owner += 1;
-                }
-                if (terrColor === main.BLACK) {
-                    owner -= 1;
-                }
+            for (first = BLACK; first <= WHITE; first++) {
+                owner += Grid.territory2owner[2 + this.grids[first].yx[j][i]];
             }
             this.territory.yx[j][i] = owner / 2.0;
         }
     }
-    if (main.debug) {
-        main.log.debug('\n+1=white, -1=black, 0=no one\n' + this.territory.toText(function (v) {
-            if (v === 0) {
-                return '    0';
-            } else {
-                return '' + '%+.1f'.format(v);
-            }
-        }));
-    }
+    if (main.debug) main.log.debug('\n+1=white, -1=black, 0=no one\n' +
+        this.territory.toText(function (v) {return v === 0 ? '    0' : '%+.1f'.format(v); }));
     return this.territory.yx;
 };
 
@@ -80,27 +69,21 @@ PotentialTerritory.prototype.foresee = function (grid, first, second) {
     this.enlarge(this.realGrid, this.tmp.copy(this.realGrid), first, second);
     this.enlarge(this.tmp, grid.copy(this.tmp), second, first);
     this.connectToBorders(grid.yx);
-    if (main.debug) {
-        main.log.debug('after 1st enlarge:\n' + grid);
-    }
+    if (main.debug) main.log.debug('after 1st enlarge:\n' + grid);
+
     // for reducing we start from the enlarged grid
     this.reduce(this.reducedGrid.copy(grid));
     this.reducedYx = this.reducedGrid.yx;
-    if (main.debug) {
-        main.log.debug('after reduce:\n' + this.reducedGrid);
-    }
+    if (main.debug) main.log.debug('after reduce:\n' + this.reducedGrid);
+
     // now we have the reduced goban, play the enlarge moves again minus the extra
     this.enlarge(this.realGrid, this.tmp.copy(this.realGrid), first, second);
     this.enlarge(this.tmp, grid.copy(this.tmp), second, first);
     this.connectToBorders(grid.yx);
-    if (main.debug) {
-        main.log.debug('after 2nd enlarge:');
-    }
-    if (main.debug) {
-        this.goban.debugDisplay();
-    }
+    if (main.debug) main.log.debug('after 2nd enlarge:\n' + grid);
+
     // passed grid will receive the result (scoring grid)
-    this.boan.countScore(this.goban, grid.convert(this.goban.grid));
+    this.boan.countScore(this.goban, grid.initFromGoban(this.goban));
     // restore goban
     moveCount = this.goban.moveNumber() - moveCount;
     for (var _i = 0; _i < moveCount; _i++) {
@@ -109,14 +92,12 @@ PotentialTerritory.prototype.foresee = function (grid, first, second) {
 };
 
 PotentialTerritory.prototype.enlarge = function (inGrid, outGrid, first, second) {
-    if (main.debug) {
-        main.log.debug('enlarge ' + first + ',' + second);
-    }
+    if (main.debug) main.log.debug('enlarge ' + first + ',' + second);
     var inYx = inGrid.yx;
     var outYx = outGrid.yx;
     for (var j = 1; j <= this.gsize; j++) {
         for (var i = 1; i <= this.gsize; i++) {
-            if (inYx[j][i] !== main.EMPTY) {
+            if (inYx[j][i] !== EMPTY) {
                 continue;
             }
             this.enlargeAt(inYx, outYx, i, j, first, second);
@@ -129,17 +110,17 @@ PotentialTerritory.prototype.reduce = function (grid) {
     var yx = grid.yx;
     for (var j = 1; j <= this.gsize; j++) {
         for (var i = 1; i <= this.gsize; i++) {
-            if (this.realYx[j][i] !== main.EMPTY) { // cannot reduce a real stone
-                continue;
-            }
+            // cannot reduce a real stone:
+            if (this.realYx[j][i] !== EMPTY) continue;
+            
             var color = yx[j][i];
-            if (color === main.EMPTY) { // we did not enlarge here, no need to reduce
-                continue;
-            }
+            // if we did not enlarge here, no need to reduce
+            if (color === EMPTY) continue;
+
             var enemies = this.inContact(yx, i, j, 1 - color);
             // we can safely reduce if no enemy was around at the end of the enlarging steps
             if (enemies === 0) {
-                yx[j][i] = main.EMPTY;
+                yx[j][i] = EMPTY;
             }
         }
     }
@@ -164,7 +145,7 @@ PotentialTerritory.prototype.enlargeAt = function (inYx, outYx, i, j, first, sec
 PotentialTerritory.prototype.addStone = function (yx, i, j, color) {
     if (this.reducedYx) {
         // skip if useless move (it was reduced)
-        if (this.reducedYx[j][i] === main.EMPTY) {
+        if (this.reducedYx[j][i] === EMPTY) {
             return;
         }
         // we check only against sucicide (e.g. no need to check against ko or non empty)
@@ -213,7 +194,7 @@ PotentialTerritory.prototype.connectToBorders = function (yx) {
         for (var c, c_array = PotentialTerritory.AROUND, c_ndx = 0; c=c_array[c_ndx], c_ndx < c_array.length; c_ndx++) {
             var i = (( c[0] < 0 ? this.gsize : c[0] * n )) + c[1]; // n,1,n,gsize
             var j = (( c[2] < 0 ? this.gsize : c[2] * n )) + c[3]; // 1,n,gsize,n
-            if (yx[j][i] === main.EMPTY) {
+            if (yx[j][i] === EMPTY) {
                 var i2 = (( c[0] < 0 ? this.gsize - 1 : c[0] * n )) + c[1] * 2; // n,2,n,gsize-1
                 var j2 = (( c[2] < 0 ? this.gsize - 1 : c[2] * n )) + c[3] * 2; // 2,n,gsize-1,n
                 var i3 = (( c[0] < 0 ? this.gsize : c[0] * (n + 1) )) + c[1]; // n+1,1,n+1,gsize
@@ -221,7 +202,7 @@ PotentialTerritory.prototype.connectToBorders = function (yx) {
                 var i4 = (( c[0] < 0 ? this.gsize : c[0] * (n - 1) )) + c[1]; // n-1,1,n-1,gsize
                 var j4 = (( c[2] < 0 ? this.gsize : c[2] * (n - 1) )) + c[3]; // 1,n-1,gsize,n-1
                 var next2border = yx[j2][i2];
-                if (next2border !== main.EMPTY && yx[j3][i3] === main.EMPTY && yx[j4][i4] === main.EMPTY) {
+                if (next2border !== EMPTY && yx[j3][i3] === EMPTY && yx[j4][i4] === EMPTY) {
                     this.addStone(yx, i, j, next2border);
                 }
             }
