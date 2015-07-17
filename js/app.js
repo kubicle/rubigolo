37b7b0c121efc540d945cfff5f6d323133d3ada0
kubicle <kubicle@yahoo.com>
2015-07-17 23:49:15 +0900

    Minor refact

--- js/app.js
+++ js/app.js
@@ -3,21 +3,17 @@
 var main = require('./main');
 window.main = main;
 
+// Constants attached to main and extensions of common classes
 require('./StoneConstants');
 require('./rb');
-
-main.GameLogic = require('./GameLogic');
-main.Grid = require('./Grid');
-main.Ai1Player = require('./ai/Ai1Player');
-main.ScoreAnalyser = require('./ScoreAnalyser');
-
-var Ui = require('./Ui');
-var TestUi = require('./TestUi');
 
 // Include tests in main build
 require('./test/TestAll');
 
 main.debug = false;
+
+var Ui = require('./Ui');
+var TestUi = require('./TestUi');
 
 var ui = window.unitTest ? new TestUi() : new Ui();
 ui.createUi();
