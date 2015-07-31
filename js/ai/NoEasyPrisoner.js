6d6b8e495f0ac31561c81426bfa13071ae6fee1f
kubicle <kubicle@yahoo.com>
2015-08-01 00:37:57 +0900

    Added evalBoard

--- js/ai/NoEasyPrisoner.js
+++ js/ai/NoEasyPrisoner.js
@@ -1,16 +1,21 @@
 //Translated from no_easy_prisoner.rb using babyruby2js
 'use strict';
 
-var inherits = require('util').inherits;
-var Stone = require('../Stone');
 var main = require('../main');
-// Should recognize when our move is foolish...
+
+var Grid = require('../Grid');
 var Heuristic = require('./Heuristic');
 var Hunter = require('./Hunter');
+var inherits = require('util').inherits;
+var Stone = require('../Stone');
 
-/** @class */
+var sOK = main.sOK;
+
+
+/** @class Should recognize when our move is foolish... */
 function NoEasyPrisoner(player) {
     Heuristic.call(this, player);
+    this.executioner = player.getHeuristic('Executioner');
     this.enemyHunter = new Hunter(player, true);
 }
 inherits(NoEasyPrisoner, Heuristic);
@@ -21,26 +26,37 @@ NoEasyPrisoner.prototype.initColor = function () {
     return this.enemyHunter.initColor();
 };
 
+NoEasyPrisoner.prototype.evalBoard = function (stateYx, scoreYx) {
+    var myScoreYx = this.scoreGrid.yx;
+    var executionerYx = this.executioner.scoreGrid.yx;
+    for (var j = 1; j <= this.gsize; j++) {
+        for (var i = 1; i <= this.gsize; i++) {
+            if (stateYx[j][i] < sOK) continue;
+            var score = myScoreYx[j][i] = this.evalMove(i, j);
+            if (score === 0) continue;
+            // Remove score given by Executioner if this move does not work
+            // var execScore = executionerYx[j][i];
+            // if (execScore < -score) score -= execScore;
+            scoreYx[j][i] += score;
+        }
+    }
+};
+
 NoEasyPrisoner.prototype.evalMove = function (i, j) {
     // NB: snapback is handled in hunter; here we just notice the sacrifice of a stone, which will
     // be balanced by the profit measured by hunter (e.g. lose 1 but kill 3).
     var stone = Stone.playAt(this.goban, i, j, this.color);
     var g = stone.group;
-    var score = 0;
+    var score = 0, move;
+    if (main.debug) move = Grid.xy2move(i, j);
     if (g.lives === 1) {
         score = - this.groupThreat(g);
-        if (main.debug) {
-            main.log.debug('NoEasyPrisoner says ' + i + ',' + j + ' is plain foolish (' + score + ')');
-        }
+        if (main.debug) main.log.debug('NoEasyPrisoner says ' + move + ' is plain foolish (' + score + ')');
     } else if (g.lives === 2) {
-        if (main.debug) {
-            main.log.debug('NoEasyPrisoner asking Hunter to look at ' + i + ',' + j);
-        }
+        if (main.debug) main.log.debug('NoEasyPrisoner asking Hunter to look at ' + move);
         if (this.enemyHunter.escapingAtariIsCaught(stone)) {
             score = - this.groupThreat(g);
-            if (main.debug) {
-                main.log.debug('NoEasyPrisoner (backed by Hunter) says ' + i + ',' + j + ' is foolish  (' + score + ')');
-            }
+            if (main.debug) main.log.debug('NoEasyPrisoner (backed by Hunter) says ' + move + ' is foolish  (' + score + ')');
         }
     }
     Stone.undo(this.goban);
