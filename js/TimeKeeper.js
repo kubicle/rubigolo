c0aab5a2b76fa621b8def4f1d7cfae2ec333664a
kubicle <kubicle@yahoo.com>
2015-10-12 19:38:09 +0900

    Refact TimeKeeper

--- js/TimeKeeper.js
+++ js/TimeKeeper.js
@@ -3,15 +3,16 @@
 
 var main = require('./main');
 
+
 /** @class tolerance allows you to ignore a bad performance to some extent. E.g 1.05 gives you 5% tolerance up
  *  ratio allows you to adapt to slower or faster system. E.g 1.0 if your system is as slow as mine :(
  */
 function TimeKeeper(tolerance, ratio) {
-    if (tolerance === undefined) tolerance = 1.15;
-    if (ratio === undefined) ratio = 1.0;
-    this.tolerance = tolerance;
-    this.ratio = ratio;
+    this.tolerance = tolerance !== undefined ? tolerance : 1.15;
+    this.ratio = ratio !== undefined ? ratio : 1.0;
     this.log = main.log;
+
+    this.duration = this.taskName = this.expectedTime = this.t0 = undefined;
 }
 module.exports = TimeKeeper;
 
@@ -20,7 +21,7 @@ module.exports = TimeKeeper;
 // NB: measures will always vary a bit unless we find the perfect calibration code (utopia)
 TimeKeeper.prototype.calibrate = function (expected) {
     var t0 = Date.now();
-    for (var _i = 0; _i < 2000; _i++) {
+    for (var i = 0; i < 2000; i++) {
         var m = {};
         for (var n = 0; n < 100; n++) {
             m[n.toString()] = n;
@@ -44,37 +45,37 @@ TimeKeeper.prototype.calibrate = function (expected) {
 // the expected time given will be adjusted according to the current calibration
 TimeKeeper.prototype.start = function (taskName, expectedInSec) {
     this.taskName = taskName;
-    this.expectedTime = expectedInSec * this.ratio;
+    this.expectedTime = expectedInSec ? expectedInSec * this.ratio : undefined;
     this.log.info('Started "' + taskName + '"...');
     this.t0 = Date.now();
 };
 
-// Stops timing, displays the report and raises exception if we went over limit
-// Unless raise_if_overlimit is false, in which case we would simply log and return the error message
-TimeKeeper.prototype.stop = function (raiseIfOverlimit) {
-    if (raiseIfOverlimit === undefined) raiseIfOverlimit = true;
+// Stops timing, displays the report and logs an error if we went over limit,
+// unless lenientIfSlow is true, in which case we would simply log and return the error message
+TimeKeeper.prototype.stop = function (lenientIfSlow) {
     this.duration = (Date.now() - this.t0) / 1000;
     this.log.info(' => ' + this.resultReport());
-    return this.checkLimits(raiseIfOverlimit);
+    return this._checkLimits(lenientIfSlow);
 };
 
 TimeKeeper.prototype.resultReport = function () {
-    var s = '';
-    s += 'Measuring "' + this.taskName + '":';
-    s += ' time: ' + this.duration.toFixed(2) + 's (expected ' +
-        this.expectedTime.toFixed(2) + ' hence ' +
-        (this.duration / this.expectedTime * 100).toFixed(2) + '%)';
-    return s;
+    var report = 'Measuring "' + this.taskName + '": time: ' + this.duration.toFixed(2) + 's';
+    if (this.expectedTime) {
+        report += ' (expected ' + this.expectedTime.toFixed(2) + ' hence ' +
+            (this.duration / this.expectedTime * 100).toFixed(2) + '%)';
+    }
+    return report;
 };
 
-//private;
-TimeKeeper.prototype.checkLimits = function (raiseIfOverlimit) {
-    if (this.duration > this.expectedTime * this.tolerance) {
-        var msg1 = 'Duration over limit: ' + this.duration.toFixed(2);
-        if (raiseIfOverlimit) {
-            throw new Error(msg1);
-        }
-        return msg1;
+TimeKeeper.prototype._checkLimits = function (lenientIfSlow) {
+    if (!this.expectedTime) return '';
+    if (this.duration <= this.expectedTime * this.tolerance) return '';
+
+    var msg = 'Duration over limit: ' + this.duration.toFixed(2) +
+        ' instead of ' + this.expectedTime.toFixed(2);
+    if (!lenientIfSlow) {
+        main.tests.warningCount++;
+        this.log.error(this.taskName + ': ' + msg);
     }
-    return '';
+    return msg;
 };
