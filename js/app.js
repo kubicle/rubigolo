8a1dcf33fb2af83b2a8b4f9ac5f3c26aa04122f6
kubicle <kubicle@yahoo.com>
2015-10-12 20:01:59 +0900

    minor

--- js/app.js
+++ js/app.js
@@ -27,6 +27,7 @@ if (typeof window === 'undefined') {
     
     var failCount = main.tests.run(logfn);
     if (failCount === 0) process.exit(0);
+
     console.error('Unit tests failed: ' + failCount + ' issue(s)');
     process.exit(1); // code != 0 means error here
 }
