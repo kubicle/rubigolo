b372f4c669b6ff6ce3e2639648b7b229e028891b
kubicle <kubicle@yahoo.com>
2015-10-29 00:48:05 +0900

    Minor refact ZoneFiller

--- js/ai/droopy/boan/ZoneFiller.js
+++ js/ai/droopy/boan/ZoneFiller.js
@@ -3,18 +3,17 @@
 
 var main = require('../../../main');
 
+var BORDER = main.BORDER, WHITE = main.WHITE;
+
 
 /** @class public read-only attribute: grid
  *  if a grid is given, it is used as starting point; 
- *  otherwise, the goban scoring_grid is used.
+ *  otherwise, goban.scoringGrid is used.
  */
 function ZoneFiller(goban, grid) {
-    if (!grid) {
-        grid = goban.scoringGrid.initFromGoban(goban);
-    }
     this.goban = goban;
-    this.grid = grid;
-    this.yx = grid.yx;
+    this.grid = grid || goban.scoringGrid.initFromGoban(goban);
+    this.yx = this.grid.yx;
     this.groups = null;
 
     this.toReplace = this.groups = null;
@@ -23,12 +22,12 @@ module.exports = ZoneFiller;
 
 
 // "Color" a goban zone.
-// to_replace can be EMPTY or a zone code (but cannot be a real color like BLACK)
+// toReplace can be EMPTY or a zone code (but cannot be a real color like BLACK)
 // neighbors, if given should be an array of n arrays, with n == number of colors
 // if neighbors are not given, we do simple "coloring"
 ZoneFiller.prototype.fillWithColor = function (startI, startJ, toReplace, color, neighbors) {
     if (this.yx[startJ][startI] !== toReplace) return 0;
-    var vcount = 0;
+    var vcount = 0, yx = this.yx;
     this.toReplace = toReplace;
     this.groups = neighbors;
     var gap, gaps = [[startI, startJ, startJ]];
@@ -36,7 +35,7 @@ ZoneFiller.prototype.fillWithColor = function (startI, startJ, toReplace, color,
     while ((gap = gaps.pop())) {
         var i = gap[0], j0 = gap[1], j1 = gap[2];
         
-        if (this.yx[j0][i] !== toReplace) continue; // gap already done by another path
+        if (yx[j0][i] !== toReplace) continue; // gap already done by another path
 
         while (this._check(i, j0 - 1)) j0--;
         while (this._check(i, j1 + 1)) j1++;
@@ -46,7 +45,9 @@ ZoneFiller.prototype.fillWithColor = function (startI, startJ, toReplace, color,
         for (var ix = i - 1; ix <= i + 1; ix += 2) {
             var curgap = null;
             for (var j = j0; j <= j1; j++) {
-                if (ix < i) this.yx[j][i] = color;
+                if (ix < i) {
+                    yx[j][i] = color; // fill with color
+                }
                 if (this._check(ix, j)) {
                     if (!curgap) {
                         // New gap in ix starts at j
@@ -66,14 +67,16 @@ ZoneFiller.prototype.fillWithColor = function (startI, startJ, toReplace, color,
 // Returns true if the replacement is needed (=> i,j has a color equal to the replaced one)
 ZoneFiller.prototype._check = function (i, j) {
     var color = this.yx[j][i];
-    if (color === main.BORDER) return false;
+    if (color === BORDER) return false;
     if (color === this.toReplace) return true;
 
-    if (this.groups && color < 2) {
-        var group = this.goban.stoneAt(i, j).group;
-        if (group && this.groups[color].indexOf(group) < 0) {
-            this.groups[color].push(group);
-        }
-    }
+    if (!this.groups) return false; // we don't want the groups surrounding zones
+    if (color > WHITE) return false; // i,j is part of a void
+    var group = this.goban.stoneAt(i, j).group;
+    if (!group) throw new Error('Unexpected: ZoneFiller replacing a group'); // (since i,j is EMPTY)
+    var groups = this.groups[color];
+    if (groups.indexOf(group) !== -1) return false; // group is already in the list of neighbors
+
+    groups.push(group);
     return false;
 };
