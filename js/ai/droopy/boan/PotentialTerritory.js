85f8d4deaa62df455c66d387b4b7d4d00cdb917e
kubicle <kubicle@yahoo.com>
2015-11-02 23:43:27 +0900

    PotentialTeritorry does not split 3 vertex eyes anymore

--- js/ai/droopy/boan/PotentialTerritory.js
+++ js/ai/droopy/boan/PotentialTerritory.js
@@ -267,6 +267,9 @@ PotentialTerritory.prototype.connectToBorders = function (yx) {
         if (color !== EMPTY) {
             l0 = yx[gj-dx][gi-dy]; r0 = yx[gj+dx][gi+dy];
             if (l0 === color || r0 === color) continue; // no need for goal if s0 or r0
+            // special case: we don't want to split a 3 vertex eye into 2 eyes here
+            l1 = yx[s1j-dx][s1i-dy]; r1 = yx[s1j+dx][s1i+dy];
+            if (l1 === color && r1 === color) continue;
             this.addStone(yx, gi, gj, color, true);
             continue;
         }
