6b8a523b6d2488303253447fbce27a42ca5bee5c
kubicle <kubicle@yahoo.com>
2015-11-03 02:23:33 +0900

    Added corner case of connectToBorder (from prev commit)

--- js/ai/droopy/boan/PotentialTerritory.js
+++ js/ai/droopy/boan/PotentialTerritory.js
@@ -269,7 +269,7 @@ PotentialTerritory.prototype.connectToBorders = function (yx) {
             if (l0 === color || r0 === color) continue; // no need for goal if s0 or r0
             // special case: we don't want to split a 3 vertex eye into 2 eyes here
             l1 = yx[s1j-dx][s1i-dy]; r1 = yx[s1j+dx][s1i+dy];
-            if (l1 === color && r1 === color) continue;
+            if (l0 === EMPTY && r0 === EMPTY && l1 === color && r1 === color) continue;
             this.addStone(yx, gi, gj, color, true);
             continue;
         }
