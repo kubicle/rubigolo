862ded52c1aa4762edf1baa6d01b458471db125c
kubicle <kubicle@yahoo.com>
2016-01-09 15:20:02 +0900

    minor update test

--- js/test/TestGameLogic.js
+++ js/test/TestGameLogic.js
@@ -41,7 +41,7 @@ TestGameLogic.prototype.testHandicap = function () {
 TestGameLogic.prototype.testMisc = function () {
     this.game.newGame(19, 0);
     this.game.loadMoves('hand:6,f3');
-    this.assertEqual('handicap:6,f3 (1 moves)', this.game.historyString());
+    this.assertEqual('handicap:6,Wf3 (1 moves)', this.game.historyString());
     this.assertEqual([0,0], this.game.countPrisoners());
     this.assertEqual(true, this.game.playOneMove('resign'));
 };
