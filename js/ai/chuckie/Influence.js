6d2bede3a6c8391980a13dba22398605b4e0898a
kubicle <kubicle@yahoo.com>
2016-03-06 02:42:04 +0900

    Influence not caring anymore if group looks dead or not

--- js/ai/chuckie/Influence.js
+++ js/ai/chuckie/Influence.js
@@ -1,13 +1,12 @@
 'use strict';
 
-var main = require('../../main');
+var CONST = require('../../constants');
 var Grid = require('../../Grid');
 var Heuristic = require('./Heuristic');
 var inherits = require('util').inherits;
 
-var GRID_BORDER = main.GRID_BORDER;
-var EMPTY = main.EMPTY, BLACK = main.BLACK, WHITE = main.WHITE;
-var ALWAYS = main.ALWAYS;
+var GRID_BORDER = CONST.GRID_BORDER;
+var EMPTY = CONST.EMPTY, BLACK = CONST.BLACK, WHITE = CONST.WHITE;
 
 // Influence depending on distance to a stone
 var INF_AT_1 = 2, INF_AT_2 = 1; // NB: making them genes does not seem to make sense
@@ -18,8 +17,6 @@ var INF_AT_1 = 2, INF_AT_2 = 1; // NB: making them genes does not seem to make s
  */
 function Influence(player) {
     Heuristic.call(this, player);
-
-    this.deadFactor = this.getGene('deadFactor', 0.23, 0.01, 1);
 
     this.grids = [
         new Grid(this.gsize, GRID_BORDER),
@@ -43,20 +40,18 @@ Influence.prototype.evalBoard = function () {
             var color = stone.color;
             if (color === EMPTY) continue;
             var yx = this.infl[color];
-            // a dying group must have a much small influence (but maybe not 0)
-            var deadFactor = stone.group.xDead === ALWAYS ? this.deadFactor : 1;
 
             // Then we propagate it decreasingly with distance
             for (var n1, n1_array = stone.neighbors, n1_ndx = 0; n1=n1_array[n1_ndx], n1_ndx < n1_array.length; n1_ndx++) {
                 if (n1.color !== EMPTY) continue;
 
-                yx[n1.j][n1.i] += INF_AT_1 * deadFactor; // 2nd level
+                yx[n1.j][n1.i] += INF_AT_1; // 2nd level
 
                 for (var n2, n2_array = n1.neighbors, n2_ndx = 0; n2=n2_array[n2_ndx], n2_ndx < n2_array.length; n2_ndx++) {
                     if (n2.color !== EMPTY) continue;
                     if (n2 === stone) continue; // we are looking again at initial stone; skip it
 
-                    yx[n2.j][n2.i] += INF_AT_2 * deadFactor; // 3rd level
+                    yx[n2.j][n2.i] += INF_AT_2; // 3rd level
                 }
             }
         }
